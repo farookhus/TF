536628cea6e8f4973eec979e5eecdad4bbfca001
farookhus <farookhus@gmail.com>
2024-10-23 21:52:44 -0500

    rg

--- storage.tf
+++ storage.tf
@@ -2,15 +2,3 @@ resource "azurerm_resource_group" "example" {
   name     = "sfs_org"
   location = "East US"
 }
-
-resource "azurerm_storage_account" "example" {
-  name                     = "sfsorgstorageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
-}
