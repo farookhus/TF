d00b302abe648c6d044759c32f3c177879b2e2f3
farookhus <farookhus@gmail.com>
2024-10-28 23:45:36 -0500

    Test

--- defender.tf
+++ defender.tf
@@ -12,38 +12,10 @@ resource "azurerm_storage_account" "sfsdefender" {
   account_replication_type = "LRS"
 }
 
-/* resource "azapi_update_resource" "sfsdefender" {
-  type = "Microsoft.Storage/storageAccounts@2021-09-01"
-  resource_id = azurerm_storage_account.sfsdefender.id
-
-  body = {
-    properties = {
-      DnsEndpointType = "AzureDnsZone"
-    }
-  }
-} */
-
-
-  resource "azapi_update_resource" "sfsdefender" {
+resource "azapi_resource" "sfsdefender" {
   type = "Microsoft.Security/defenderForStorageSettings@2022-12-01-preview"
   name = "sfsdefender"
   parent_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab"
-  body = {
-    properties = {
-      isEnabled = false
-      malwareScanning = {
-        onUpload = {
-          capGBPerMonth = -1
-          isEnabled = false
-        }
-        scanResultsEventGridTopicResourceId = "test"
-      }
-      overrideSubscriptionLevelSettings = false
-      sensitiveDataDiscovery = {
-        isEnabled = false
-      }
-    }
-  }
 }
 
 
