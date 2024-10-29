206ee44bd6863bfdb4708ec45efbb2dbb101b22e
farookhus <farookhus@gmail.com>
2024-10-29 11:12:21 -0500

    disabled

--- defender.tf
+++ defender.tf
@@ -40,7 +40,7 @@ resource "azurerm_security_center_subscription_pricing" "sfsdefender" {
   }
 }
  */
-resource "azapi_resource_action" "enable_defender_for_Storage" {
+/* resource "azapi_resource_action" "enable_defender_for_Storage" {
   type        = "Microsoft.Security/defenderForStorageSettings@2022-12-01-preview"
   resource_id = "${azurerm_storage_account.sfsdefender.id}/providers/Microsoft.Security/defenderForStorageSettings/current"
   method      = "PUT"
@@ -60,7 +60,7 @@ resource "azapi_resource_action" "enable_defender_for_Storage" {
       overrideSubscriptionLevelSettings = false
     }
   }
-}
+} */
 
 /* resource "azurerm_security_center_storage_defender" "sfs_defender" {
   storage_account_id = azurerm_storage_account.sfs_defender.id
