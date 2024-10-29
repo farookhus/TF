3c4983dd1b145384605c9af59ac23c947efc5394
farookhus <farookhus@gmail.com>
2024-10-28 21:10:48 -0500

    Storage defender

--- defender.tf
+++ defender.tf
@@ -11,3 +11,24 @@ resource "azurerm_storage_account" "sfs_defender" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azapi_resource" "sfs_defender" {
+  type = "Microsoft.Security/defenderForStorageSettings@2022-12-01-preview"
+  name = "sfs_defender"
+  body = jsonencode({
+    properties = {
+      isEnabled = bool
+      malwareScanning = {
+        onUpload = {
+          capGBPerMonth = int
+          isEnabled = bool
+        }
+        scanResultsEventGridTopicResourceId = "string"
+      }
+      overrideSubscriptionLevelSettings = bool
+      sensitiveDataDiscovery = {
+        isEnabled = bool
+      }
+    }
+  })
+}
