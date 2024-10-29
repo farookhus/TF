ae62a556e2e237a4bb55a08c56de998c6dc0a72c
farookhus <farookhus@gmail.com>
2024-10-28 22:43:20 -0500

    Defender

--- defender.tf
+++ defender.tf
@@ -1,21 +1,21 @@
 
- resource "azurerm_resource_group" "sfs_defender" {
-   name     = "sfs_defender"
+ resource "azurerm_resource_group" "sfsdefender" {
+   name     = "sfsdefender"
    location = "East US"
  }
 
-resource "azurerm_storage_account" "sfs_defender" {
+resource "azurerm_storage_account" "sfsdefender" {
   name                     = "sfsdefenderstorage"
-  resource_group_name      = azurerm_resource_group.sfs_defender.name
-  location                 = azurerm_resource_group.sfs_defender.location
+  resource_group_name      = azurerm_resource_group.sfsdefender.name
+  location                 = azurerm_resource_group.sfsdefender.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azapi_resource" "sfs_defender" {
+resource "azapi_resource" "sfsdefender" {
   type = "Microsoft.Security/defenderForStorageSettings@2022-12-01-preview"
-  name = "sfs_defender"
-  parent_id = azurerm_resource_group.sfs_defender.id
+  name = "sfsdefender"
+  parent_id = azurerm_resource_group.sfsdefender.id
   body = {
     properties = {
       isEnabled = false
