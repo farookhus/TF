27f80ab47b0eb40bfa9879cff80d2452d6baf310
farookhus <farookhus@gmail.com>
2024-10-29 20:38:32 -0500

    Added scriptition

--- defender.tf
+++ defender.tf
@@ -67,7 +67,7 @@ resource "azurerm_security_center_subscription_pricing" "sfsdefender" {
 resource "azapi_resource" "test" {
   type = "Microsoft.Security/defenderForStorageSettings@2022-12-01-preview"
   name = "default"
-  parent_id = azurerm_resource_group.sfsdefender.id
+  parent_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab"
   body = {properties = {
     isEnabled = false
   }} 
