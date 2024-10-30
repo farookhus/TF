3f9722f63deb0cc849b648cf2579fafd9657ab91
farookhus <farookhus@gmail.com>
2024-10-29 22:23:40 -0500

    Added Parent ID

--- defender_new.tf
+++ defender_new.tf
@@ -1,5 +1,6 @@
 resource "azapi_resource" "df" {
   type = "Microsoft.Security/pricings@2024-01-01"
+  parent_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab"
   body = {properties = {
     pricingTier = "Standard"       
   }}
