a43d8aa41d4490644f071a7618f9e05e4f6bdc05
farookhus <farookhus@gmail.com>
2024-10-29 22:25:28 -0500

    Added Name

--- defender_new.tf
+++ defender_new.tf
@@ -1,6 +1,7 @@
 resource "azapi_resource" "df" {
   type = "Microsoft.Security/pricings@2024-01-01"
   parent_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab"
+  name = "standardpricing"
   body = {properties = {
     pricingTier = "Standard"       
   }}
