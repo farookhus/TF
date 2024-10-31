f7b8a3dbb56e7622a06bcd9e910ec77dfccf0847
farookhus <farookhus@gmail.com>
2024-10-30 22:14:57 -0500

    Template management

--- Defender_123.tf
+++ Defender_123.tf
@@ -24,3 +24,28 @@ TEMPLATE
 
 }
 
+resource "azurerm_management_group_template_deployment" "vmpricingmgmt" {
+  name                = "vmpricingmgmt"
+  location            = "East US"
+  management_group_id = "Farook-mgmt"
+  template_content = <<TEMPLATE
+
+{
+  "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+  "contentVersion": "1.0.0.0",
+  "resources": [
+      {
+          "type": "Microsoft.Security/pricings",
+          "apiVersion": "2018-06-01",
+          "name": "VirtualMachines",
+          "properties":{
+              "pricingTier": "Standard"
+          }
+      }      
+  ],
+  "outputs": {}
+}
+
+TEMPLATE
+
+}
