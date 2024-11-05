f13a0723a8a78800d594b1edc0a5d09374a6ccff
farookhus <farookhus@gmail.com>
2024-11-04 22:57:12 -0600

    new commit

--- Defender_123.tf
+++ Defender_123.tf
@@ -37,7 +37,26 @@ resource "azurerm_subscription_template_deployment" "defenderplannew" {
         }
       ]
     }
-  }
+  },
+                    {
+                    "type": "Microsoft.Security/pricings",
+                    "apiVersion": "2023-01-01",
+                    "name": "VirtualMachines",
+                    "properties": {
+                      "pricingTier": "Standard",
+                      "subPlan": "P2",
+                      "extensions": [
+                        {
+                          "name": "AgentlessVmScanning",
+                          "isEnabled": "true"
+                        },
+                        {
+                          "name": "MdeDesignatedSubscription",
+                          "isEnabled": "false"
+                        }
+                      ]
+                    }
+                  }
   ],
 }
 TEMPLATE
