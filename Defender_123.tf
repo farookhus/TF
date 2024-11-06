7e4279068b179cb72f259955118f113bbdad0d6a
farookhus <farookhus@gmail.com>
2024-11-05 22:26:26 -0600

    commented

--- Defender_123.tf
+++ Defender_123.tf
@@ -1,4 +1,4 @@
-
+/* 
 resource "azurerm_subscription_template_deployment" "newdefender123456" {
   name             = "newdefender123456"
   location         = "East US"
@@ -167,3 +167,4 @@ TEMPLATE
 }
 
 
+ */
