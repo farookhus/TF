ff2bcd109ea81358b3037b44b5a7680cd12313a6
farookhus <farookhus@gmail.com>
2024-11-07 15:08:27 -0600

    commit

---
+++ Defenderpricing.tf
@@ -0,0 +1,27 @@
+resource "azurerm_security_center_subscription_pricing" "Cloudposture" {
+  tier          = "Standard"
+  resource_type = "CloudPosture"
+
+  extension {
+    name = "ContainerRegistriesVulnerabilityAssessments"
+  }
+
+  extension {
+    name = "AgentlessVmScanning"
+    additional_extension_properties = {
+      ExclusionTags = "[]"
+    }
+  }
+
+  extension {
+    name = "AgentlessDiscoveryForKubernetes"
+  }
+
+  extension {
+    name = "SensitiveDataDiscovery"
+  }
+
+    extension {
+    name = "EntraPermissionsManagement"
+  }
+}
