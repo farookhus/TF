95f5c7c90c548f046f450e00d75b6dcecd20deaf
farookhus <farookhus@gmail.com>
2024-11-11 17:43:45 -0600

    module source

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
  module "mdc_plans_enable" {
-  source           = ".."
+  source           = "../"
   mdc_plans_list   = var.mdc_plans_list
   subplans         = var.subplans
   enable_telemetry = var.enable_telemetry
