29410de277c6036d349178f16b8e61a9a0a6cd60
farookhus <farookhus@gmail.com>
2024-11-11 20:15:33 -0600

    new files

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ provider "modtm" {
 }
 
 module "mdc_plans_enable" {
-  source           = "../.."
+  source           = "."
   mdc_plans_list   = var.mdc_plans_list
   subplans         = var.subplans
   enable_telemetry = var.enable_telemetry
