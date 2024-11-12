88ac16529b70a5997da2c817af138822fbead6f4
farookhus <farookhus@gmail.com>
2024-11-11 20:26:44 -0600

    source

--- variables.tf
+++ variables.tf
@@ -5,18 +5,18 @@ variable "enable_telemetry" {
 variable "mdc_plans_list" {
   type = set(string)
   default = [
-    "AppServices",
-    "Arm",
-    "CloudPosture",
-    "Containers",
-    "KeyVaults",
-    "OpenSourceRelationalDatabases",
-    "SqlServers",
-    "SqlServerVirtualMachines",
-    "CosmosDbs",
-    "StorageAccounts",
-    "VirtualMachines",
-    "Api",
+    # "AppServices",
+    # "Arm",
+     "CloudPosture",
+    # "Containers",
+    # "KeyVaults",
+    # "OpenSourceRelationalDatabases",
+    # "SqlServers",
+    # "SqlServerVirtualMachines",
+    # "CosmosDbs",
+    # "StorageAccounts",
+    # "VirtualMachines",
+    # "Api",
   ]
   description = "(Optional) Set of all MDC plans"
 }
