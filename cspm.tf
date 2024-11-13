956bbe4c157e6aed529d506a3b74ab29bcf7cae4
farookhus <farookhus@gmail.com>
2024-11-12 22:00:54 -0600

    pricingvm

--- cspm.tf
+++ cspm.tf
@@ -1,18 +1,4 @@
 
-/*  resource "azurerm_security_center_subscription_pricing" "pricingvm" {
-   tier          = "Standard"
-   resource_type = "VirtualMachines"
-
-   extension {
-     name = "AgentlessVmScanning"
-   }
-
-      extension {
-      name = "MdeDesignatedSubscription"
-    }
-
- } */
-
  resource "azurerm_security_center_subscription_pricing" "pricingcp" {
    tier          = "Standard"
    resource_type = "CloudPosture"
@@ -38,34 +24,16 @@
 
  }
 
-/* resource "azurerm_subscription_template_deployment" "cspm" {
-  name             = "cspm"
-  location         = "East US"
-  template_content = <<TEMPLATE
-{
-    "$schema": "https://schema.management.azure.com/schemas/2018-05-01/subscriptionDeploymentTemplate.json#",
-    "contentVersion": "1.0.0.0",
-    "parameters": {},
-    "functions": [],
-    "variables": {},
-    "resources": [
-     {
-      "type": "Microsoft.Security/pricings",
-       "apiVersion": "2023-01-01",
-       "name":"AppServices",
-       "properties": {
-            "pricingTier": "Standard"
-          }
-     },
-     {
-      "type": "Microsoft.Security/pricings",
-       "apiVersion": "2023-01-01",
-       "name":"VirtualMachines",
-       "properties": {
-            "pricingTier": "Standard"
-          }
-     }
-        ]            
-     }   
-TEMPLATE
-} */
+  resource "azurerm_security_center_subscription_pricing" "pricingvm" {
+   tier          = "Standard"
+   resource_type = "VirtualMachines"
+
+   extension {
+     name = "AgentlessVmScanning"
+   }
+
+      extension {
+      name = "MdeDesignatedSubscription"
+    }
+
+ } 
