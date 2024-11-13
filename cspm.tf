4d25c7c2028b4a20561499a177b85b1b062b54d5
farookhus <farookhus@gmail.com>
2024-11-12 22:15:45 -0600

    pricing

--- cspm.tf
+++ cspm.tf
@@ -38,3 +38,63 @@
     }
 
  } 
+
+   resource "azurerm_security_center_subscription_pricing" "PricingAppServices" {
+   tier          = "Standard"
+   resource_type = "AppServices"
+ } 
+
+  resource "azurerm_security_center_subscription_pricing" "pricingContainers" {
+   tier          = "Standard"
+   resource_type = "Containers"
+
+   extension {
+     name = "ContainerRegistriesVulnerabilityAssessments"
+   }
+
+      extension {
+      name = "AgentlessDiscoveryForKubernetes"
+    }
+
+          extension {
+      name = "ContainerSensor"
+    }
+
+
+ } 
+ 
+     resource "azurerm_security_center_subscription_pricing" "PricingAppsqlservervm" {
+   tier          = "Standard"
+   resource_type = "SqlServerVirtualMachines"
+ } 
+
+     resource "azurerm_security_center_subscription_pricing" "PricingSqlServers" {
+   tier          = "Standard"
+   resource_type = "SqlServers"
+ } 
+
+   resource "azurerm_security_center_subscription_pricing" "pricingkv" {
+   tier          = "Standard"
+   resource_type = "KeyVaults"
+   subplan = "PerTransaction"
+
+ } 
+
+   resource "azurerm_security_center_subscription_pricing" "pricingarm" {
+   tier          = "Standard"
+   resource_type = "Arm"
+   subplan = "PerApiCall"
+
+ } 
+
+   resource "azurerm_security_center_subscription_pricing" "pricingosrd" {
+   tier          = "Standard"
+   resource_type = "OpenSourceRelationalDatabases"
+
+ } 
+
+   resource "azurerm_security_center_subscription_pricing" "pricingCosmosDbs" {
+   tier          = "Standard"
+   resource_type = "CosmosDbs"
+
+ } 
