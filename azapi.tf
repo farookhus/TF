ec069650571e5a137f0e8c30d18740b6e02808e7
farookhus <67014644+farookhus@users.noreply.github.com>
2025-01-31 12:33:04 -0600

    malware setting

---
+++ azapi.tf
@@ -0,0 +1,21 @@
+resource "azapi_update_resource" "malwaresetting" {
+   type        = "Microsoft.Security/DefenderForStorageSettings/current/providers/Microsoft.Insights/diagnosticSettings/service?api-version=2021-05-01-preview"
+   resource_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab/resourcegroups/test/providers/microsoft.storage/storageaccounts/cocgaga123/providers/microsoft.security/defenderforstoragesettings/current/providers/microsoft.insights/diagnosticSettings/service"
+
+   body = {
+    "properties": {
+        "workspaceId": "/subscriptions/{subscriptionId}/resourcegroups/{resourceGroup}/providers/microsoft.operationalinsights/workspaces/{workspaceName}",
+        "logs": [
+            {
+                "category": "ScanResults",
+                "enabled": true,
+                "retentionPolicy": {
+                    "enabled": true,
+                    "days": 180
+                }
+            }
+        ]
+    }
+}
+
+ }
