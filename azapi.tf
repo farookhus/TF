77fda47a43f55c35d545b4be0dcb5f6cbde8542e
farookhus <67014644+farookhus@users.noreply.github.com>
2025-02-16 16:51:43 -0600

    Update azapi.tf

--- azapi.tf
+++ azapi.tf
@@ -1,3 +1,4 @@
+/*
 resource "azapi_update_resource" "malwaresetting" {
    type        = "Microsoft.Insights/diagnosticSettings@2021-05-01-preview"
    resource_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab/resourcegroups/test/providers/microsoft.storage/storageaccounts/cocgaga123/providers/microsoft.security/defenderforstoragesettings/current/providers/microsoft.insights/diagnosticSettings/service"
@@ -19,3 +20,4 @@ resource "azapi_update_resource" "malwaresetting" {
 }
 
  }
+*/
