c7ed4cfee60a2e54ae8f508eef3746ff090eeada
farookhus <67014644+farookhus@users.noreply.github.com>
2025-02-20 23:55:38 -0600

    endpoint

--- azapi.tf
+++ azapi.tf
@@ -23,7 +23,7 @@ resource "azapi_update_resource" "malwaresetting" {
 */
 
 resource "azapi_update_resource" "endpointprotection" {
-   type        = "Microsoft.Security/settings/WDATP@2022-05-01"
+   type        = "Microsoft.Security/settings"
    resource_id = "/subscriptions/a682efd9-27e6-4af8-9d10-574e2a214eab/providers/Microsoft.Security/settings/WDATP"
 
    body = {
